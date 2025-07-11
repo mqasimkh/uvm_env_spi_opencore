d9fe6f145719fbfd838afe11446fbab907295c6a
Muhammad Qasim <154578365+mqasimkh@users.noreply.github.com>
2025-07-11 11:44:54 +0500

    Delete code/wishbone/tb/xcelium.d directory

--- code/wishbone/tb/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1752216104 /home/cc/Documents/muhammad_qasim/uvm/weekly_spi_task/weekly_spi/code/wishbone/tb/tb_top.sv
-1752214549 /home/cc/Documents/muhammad_qasim/uvm/weekly_spi_task/weekly_spi/code/wishbone/sv/wish_pkg.sv
