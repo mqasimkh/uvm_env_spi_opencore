7bc600a7ee07e7bd336397f5c914a14503cbf999
Muhammad Qasim <developermqasim@outlook.com>
2025-07-11 20:32:23 +0500

    ...

--- code/wishbone/tb/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1752246331 /home/cc/Documents/muhammad_qasim/uvm/weekly_spi_task/weekly_spi/code/wishbone/tb/tb_top.sv
-1752246171 /home/cc/Documents/muhammad_qasim/uvm/weekly_spi_task/weekly_spi/code/wishbone/sv/wish_pkg.sv
