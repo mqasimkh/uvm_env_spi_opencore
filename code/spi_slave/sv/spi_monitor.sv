957c9bd15864f65252582d01a38a0e0630abb68d
Muhammad Qasim <developermqasim@outlook.com>
2025-07-13 20:21:40 +0500

    ...

--- code/spi_slave/sv/spi_monitor.sv
+++ code/spi_slave/sv/spi_monitor.sv
@@ -4,9 +4,27 @@ class spi_monitor extends uvm_monitor;
     function new (string name = "spi_monitor", uvm_component parent);
         super.new(name, parent);
     endfunction: new
+    
+    virtual spis_if vif;
+    spi_packet spkt;
 
     function void build_phase (uvm_phase phase);
         `uvm_info(get_type_name(), "BUILD PHASE RUNNING ...", UVM_LOW)
     endfunction: build_phase
     
+    task run_phase (uvm_phase phase);
+        forever begin
+            collect_data();
+        end
+    endtask: run_phase
+
+    task collect_data();
+        @(posedge vif.sck_o);
+
+        for (int i = 8; i > 0; i--) begin
+            data_in[i] = vif.mosi;
+        end
+
+    endtask: collect_data
+
 endclass: spi_monitor
---
+++ code/spi_slave/sv/spis_if.sv
@@ -0,0 +1,8 @@
+interface spis_if();
+
+  logic sck_o,
+  logic ss_o,
+  logic mosi_o,
+  logic miso_i    
+
+endinterface: spis_if
